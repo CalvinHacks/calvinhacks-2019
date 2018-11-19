400b6fcebae37ab3d4cd5f1505cd0284fe18a213
Ian Park <duckonomy@gmail.com>
2018-11-18 21:45:53 -0500

    hover

--- public/main.js
+++ public/main.js
@@ -1,35 +1,35 @@
-//////////
-// Date //
-//////////
+// //////////
+// // Date //
+// //////////
 
-// Set the date we're counting down
-var countDownDate = new Date("Feb 7, 2019 15:37:25").getTime();
+// // Set the date we're counting down
+// var countDownDate = new Date("Feb 7, 2019 15:37:25").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// // Update the count down every 1 second
+// var x = setInterval(function() {
 
-  // Get todays date and time
-  var now = new Date().getTime();
+//   // Get todays date and time
+//   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="time"
-  document.getElementById("time").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+//   // Display the result in the element with id="time"
+//   document.getElementById("time").innerHTML = days + "d " + hours + "h "
+//     + minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("time").innerHTML = "EXPIRED";
-  }
-}, 1000);
+//   // If the count down is finished, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("time").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
 
 
 
@@ -40,22 +40,22 @@ $(document).ready(function() {
   // Mail //
   //////////
 
-  $('form').submit(function(event) {
-    var userEmail = $('#email').val();
+  // $('form').submit(function(event) {
+  //   var userEmail = $('#email').val();
 
-    event.preventDefault();
+  //   event.preventDefault();
 
-    $.ajax({
-      url: '/',
-      type: 'POST',
-      data: {
-        email: userEmail
-      },
-      success: function(response) {
-        console.log(response);
-      }
-    });
-  });
+  //   $.ajax({
+  //     url: '/',
+  //     type: 'POST',
+  //     data: {
+  //       email: userEmail
+  //     },
+  //     success: function(response) {
+  //       console.log(response);
+  //     }
+  //   });
+  // });
 
 
   ////////////
