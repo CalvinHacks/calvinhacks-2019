81613a0935a376b68e80651a14a09e0e2eef5d9e
Ian Park <duckonomy@gmail.com>
2019-01-30 21:18:16 +0000

    changes

--- public/main.js
+++ public/main.js
@@ -1,66 +1,5 @@
-// //////////
-// // Date //
-// //////////
-
-// // Set the date we're counting down
-// var countDownDate = new Date("Feb 7, 2019 15:37:25").getTime();
-
-// // Update the count down every 1 second
-// var x = setInterval(function() {
-
-//   // Get todays date and time
-//   var now = new Date().getTime();
-
-//   // Find the distance between now and the count down date
-//   var distance = countDownDate - now;
-
-//   // Time calculations for days, hours, minutes and seconds
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-//   // Display the result in the element with id="time"
-//   document.getElementById("time").innerHTML = days + "d " + hours + "h "
-//     + minutes + "m " + seconds + "s ";
-
-//   // If the count down is finished, write some text
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById("time").innerHTML = "EXPIRED";
-//   }
-// }, 1000);
-
-
-
 // Email subscription to Mailchimp using jquery
 $(document).ready(function() {
-
-  //////////
-  // Mail //
-  //////////
-
-  // $('form').submit(function(event) {
-  //   var userEmail = $('#email').val();
-
-  //   event.preventDefault();
-
-  //   $.ajax({
-  //     url: '/',
-  //     type: 'POST',
-  //     data: {
-  //       email: userEmail
-  //     },
-  //     success: function(response) {
-  //       console.log(response);
-  //     }
-  //   });
-  // });
-
-
-  ////////////
-  // Scroll //
-  ////////////
 
   $(".fa-angle-double-down").click(function() {
     $('html,body').animate({
@@ -83,5 +22,51 @@ $(document).ready(function() {
       } 
     });
   }
+
+  ///////////
+  // Video //
+  ///////////
+
+  // function isiPhone() {
+  //   return (
+  //     (navigator.platform.indexOf("iPhone") != -1) ||
+  //     (navigator.platform.indexOf("iPod") != -1)
+  //   );
+  // }
+
+  // if (isiPhone()) {
+  //   $('.video-container').remove();
+  //   $('.front-page').addClass('still-smaller');
+  // }
+
+  // if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
+  //   $('.video-container').remove();
+  //   $('.front-page').addClass('still-smaller');
+  // }
+
+  $(window).on("load resize", function() {
+    if ($(window).width() > 2048 || $(window).height() > 1080) {
+      $('.video-container').hide();
+      if ($(window).width() < 3840) {
+        $('.front-page').removeClass('still');
+        $('.front-page').addClass('still-smaller');
+      } else {
+        $('.front-page').removeClass('still-smaller');
+        $('.front-page').addClass('still');
+      }
+    } else {
+      if ($(window).width() < 900) {
+        $('.video-container').hide();
+        $('.front-page').addClass('still');
+        $('.front-page').removeClass('still-smaller');
+      } else {
+        $('.video-container').show();
+        $('.front-page').removeClass('still');
+        $('.front-page').removeClass('still-smaller');
+      }
+    }
+  });
+
+
 
 });
--- server.js
+++ server.js
@@ -11,33 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 app.post('/', (req, res) => {
-  console.log(req.body);
-  addMailToList(req.body.email);
   res.end();
 });
-
-function addMailToList(addr) {
-  var request = require("request");
-
-  var options = {
-    method: 'POST',
-    url: 'https://us19.api.mailchimp.com/3.0/lists/8ca0184cb7/members',
-    headers:
-    {
-      'Postman-Token': '1bacb8f0-3e0b-4a2c-bd73-401eab59d76f',
-      'Cache-Control': 'no-cache',
-      Authorization: 'Basic c290bHBhcmtAZ21haWwuY29tOmY4MzJmODUwMDJkZThlOTE3NzE0Njc2ZDE2MWNjYTFlLQ==',
-      'Content-Type': 'application/json'
-    },
-    body: { email_address: addr, status: 'subscribed' },
-    json: true
-  };
-
-  request(options, function(error, response, body) {
-    if (error) throw new Error(error);
-    console.log(body);
-  });
-}
 
 const port = process.env.PORT || 4000;
 app.set('port', port);
